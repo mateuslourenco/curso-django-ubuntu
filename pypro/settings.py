2efb2ab61a1aa28540f116aaf301d5f2237fe578
mateus <mateuslourenco55@outlook.com>
2021-12-22 00:07:29 -0300

    Extraída variável de ambiente SECRET_KEY
    close #15

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pyy4+g64$w0x^io26h*(9n5g1_t*eu^cm*j!0ijrzp68lk5q+a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
