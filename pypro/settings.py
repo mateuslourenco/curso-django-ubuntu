c3e15da6e6a3666594f21fae282953cf5178dca7
mateus <mateuslourenco55@outlook.com>
2021-12-21 23:56:42 -0300

    Criada infra decouple
    close #13

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pyy4+g64$w0x^io26h*(9n5g1_t*eu^cm*j!0ijrzp68lk5q+a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
