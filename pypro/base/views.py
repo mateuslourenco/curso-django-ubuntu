dbfff9707c822cdbf4f5f20f3d63d5940decfff4
mateus <mateuslourenco55@outlook.com>
2022-01-05 11:59:00 -0300

    Instalado arquivos estáticos localmente

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body><h1>Olá Django</h1></body></html>')
+    return render(request, 'base/home.html')
