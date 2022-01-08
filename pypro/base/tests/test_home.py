e8fd94839aa47a1b2b8fc8acfbafc2700e3b8673
mateus <mateuslourenco55@outlook.com>
2022-01-08 20:07:04 -0300

    Implementada fixture resp

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,13 +1,21 @@
+import pytest
 from django.test import Client
-
 from pypro.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
+@pytest.fixture
+def resp(client):
     resp = client.get('/')
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title(client: Client):
-    resp = client.get('/')
+def test_title(resp):
     assert_contains(resp, '<title>Python Pro</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, 'href="/">Python Pro')
