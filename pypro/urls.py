91f35c391cb77f999ee2ce48918c7ff5cacb135d
mateus <mateuslourenco55@outlook.com>
2022-01-16 15:02:08 -0300

    Criada app aperitivos close #92

--- pypro/urls.py
+++ pypro/urls.py
@@ -22,6 +22,7 @@ from pypro import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
+    path('aperitivos/', include('pypro.aperitivos.urls'))
 ]
 
 if settings.DEBUG:
