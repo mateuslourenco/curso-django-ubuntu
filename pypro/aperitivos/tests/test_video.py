b3aeaf61c85f1f8038c59ba78caade532efe4fa3
mateus <mateuslourenco55@outlook.com>
2022-01-19 00:04:51 -0300

    adicionado teste para video nao existente
    close #118

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -17,8 +17,17 @@ def resp(client, video):
     return client.get(reverse('aperitivos:video', args=(video.slug,)))
 
 
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug + 'video_nao_existente',)))
+
+
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_status_code_pagina_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 
 def test_titulo_video(resp, video):
