aea273861dd089675c11e733a5e53b51c923929e
mateus <mateuslourenco55@outlook.com>
2022-01-20 03:18:01 -0300

    atualizado asser test_listar_modulos_ordenados

--- pypro/modulos/tests/test_facade.py
+++ pypro/modulos/tests/test_facade.py
@@ -7,8 +7,8 @@ from pypro.modulos.models import Modulo
 
 @pytest.fixture
 def modulos(db):
-    return [mommy.make(Modulo, titulo=s) for s in 'Depois Antes'.split()]
+    return [mommy.make(Modulo, titulo=s) for s in 'Antes Depois'.split()]
 
 
 def test_listar_modulos_ordenados(modulos):
-    assert list(sorted(modulos, key=lambda modulo: modulo.titulo)) == facade.listar_modulos_ordenados()
+    assert list(sorted(modulos, key=lambda modulo: modulo.order)) == facade.listar_modulos_ordenados()
