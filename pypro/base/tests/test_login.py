7f58d25c1dc1aa146ab294051ac2049a61588dfd
mateus <mateuslourenco55@outlook.com>
2022-01-23 18:42:48 -0300

    Implementação de Logout
    close #176

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -66,3 +66,7 @@ def test_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_lo
 
 def test_botao_sair_disponivel(resp_home_com_usuario_logado):
     assert_contains(resp_home_com_usuario_logado, 'Sair')
+
+
+def test_link_de_logout_disponivel(resp_home_com_usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, reverse('logout'))
