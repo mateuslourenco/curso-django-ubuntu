89864cf488bc519366ebe768c43577eeb33b49ec
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-15 00:56:41 -0300

    criado teste para validar form presente

--- pypro/base/tests/test_registrar.py
+++ pypro/base/tests/test_registrar.py
@@ -5,6 +5,11 @@ from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
+def resp(client, db):
+    return client.get(reverse('base:registrar'))
+
+
+@pytest.fixture
 def resp_home(client, db):
     return client.get(reverse('login'))
 
@@ -15,3 +20,11 @@ def test_botao_registrar_disponivel(resp_home):
 
 def test_link_registrar_disponivel(resp_home):
     assert_contains(resp_home, reverse('base:registrar'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_form_presente(resp):
+    assert_contains(resp, '<form')
