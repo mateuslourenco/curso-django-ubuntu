fbe2ac3c9110a514d5c6a6ef33493b1b671c0228
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-15 22:57:28 -0300

    criado formulario de registro

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,4 +1,9 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from pypro.base.forms import SignUpForm
 
 
 def home(request):
@@ -6,4 +11,19 @@ def home(request):
 
 
 def registrar(request):
-    return render(request, 'registration/signup.html')
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('email')
+                password = form.cleaned_data.get('password2')
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return HttpResponseRedirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
